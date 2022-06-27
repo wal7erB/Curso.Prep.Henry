1431f19b9e05b990abcc9dc5d0cc901539d69741
wal7erb <walterbritos418@gmail.com>
2022-06-27 10:08:23 -0300

    JS extras terminado

--- 10-JS-Extra-Homeworks/homework/homework.js
+++ 10-JS-Extra-Homeworks/homework/homework.js
@@ -10,6 +10,11 @@ function deObjetoAmatriz(objeto){
       C: 3
     }) ➞ [["D", 1], ["B", 2], ["C", 3]]*/
   //Escribe tu código aquí
+var nuevoArray = [];
+for(var clave in objeto){
+  nuevoArray.push([clave,objeto[clave]]);
+}
+return nuevoArray;
 }
 
 
@@ -18,7 +23,17 @@ function numberOfCharacters(string) {
   //en formato par clave-valor.
   //Ej: Recibe ---> "adsjfdsfsfjsdjfhacabcsbajda" || Devuelve ---> { a: 5, b: 2, c: 2, d: 4, f: 4, h:1, j: 4, s: 5 } 
   //Escribe tu código aquí
+  var objeto = {};
+  for(var i = 0; i <string.length; i++){
+    if(objeto.hasOwnProperty(string[i])){
+      objeto[string[i]] = objeto[string[i]] + 1;
+    }else{
+      objeto[string[i]] = 1;
+    }
+  }
+  return objeto;
 }
+
 
 
 function capToFront(s) {
@@ -26,8 +41,18 @@ function capToFront(s) {
   //al principio de la palabra.
   //Ejemplo: soyHENRY -> HENRYsoy
   //Escribe tu código aquí
+  var acumuladorMayusculas = "";
+  var acumuladorMinusculas = "";
+  for(var i = 0; i < s.length; i++ ){
+    if(s[i] === s[i].toLowerCase()){
+      acumuladorMinusculas = acumuladorMinusculas + s[i];
+    }
+    else{
+     acumuladorMayusculas = acumuladorMayusculas + s[i]
+    }
+  }
+  return acumuladorMayusculas + acumuladorMinusculas;
 }
-
 
 function asAmirror(str) {
   //La función recibe una frase. 
@@ -35,6 +60,11 @@ function asAmirror(str) {
   //pero con cada una de sus palabras invertidas, como si fuera un espejo.
   //Ej: Recibe ---> "The Henry Challenge is close!" || Devuelve ---> "ehT yrneH egnellahC si !esolc"
   //Escribe tu código aquí
+  var strToArray = str.split(" ");
+  var arrayFinal = strToArray.map(function(ele){
+    return ele.split("").reverse().join("");
+  });
+  return arrayFinal.join(" ");
 } 
 
 
@@ -43,6 +73,13 @@ function capicua(numero){
   //La misma debe retornar: "Es capicua" si el número se número que se lee igual de 
   //izquierda a derecha que de derecha a izquierda. Caso contrario retorna "No es capicua"
   //Escribe tu código aquí
+ var numeroAStr = numero.toString()
+ var resto = numeroAStr.split("").reverse().join("");
+ if(numeroAStr === resto){
+  return "Es capicua" 
+ }else {
+  return "No es capicua"
+ }
 }
 
 
@@ -50,6 +87,16 @@ function deleteAbc(cadena){
   //Define una función que elimine las letras "a", "b" y "c" de la cadena dada 
   //y devuelva la versión modificada o la misma cadena, en caso de contener dichas letras.
   //Escribe tu código aquí
+  var sinAbc = "";
+  for(var i = 0; i < cadena.length; i++){
+    if(cadena[i] === "a" || cadena[i] === "b" || cadena[i] === "c"){
+     continue;
+    }
+    else{
+      sinAbc = sinAbc + cadena[i]
+    }
+  }
+  return sinAbc
 }
 
 
@@ -57,8 +104,20 @@ function sortArray(arr) {
   //La función recibe una matriz de strings. Ordena la matriz en orden creciente de longitudes de cadena
   //Ej: Recibe ---> ["You", "are", "beautiful", "looking"] || Devuelve ---> [“You", "are", "looking", "beautiful"]
   //Escribe tu código aquí
+  var cambio = true;
+  while(cambio){
+    cambio = false;
+    for(var i = 0; i < arr.length - 1; i++){
+      if (arr[i].length > arr[i+1].length){
+        var auxiliar = arr[i];
+         arr[i] = arr[i+1];
+         arr[i+1] = auxiliar;
+         cambio = true;
+      }
+    }
+  }
+  return arr;
 }
-
 
 function buscoInterseccion(arreglo1, arreglo2){
   //Existen dos arrays, cada uno con 5 números. A partir de ello, escribir una función que permita 
@@ -66,6 +125,15 @@ function buscoInterseccion(arreglo1, arreglo2){
   //Si no tienen elementos en común, retornar un arreglo vacío.
   //Aclaración: los arreglos no necesariamente tienen la misma longitud
   //Escribe tu código aquí  
+  var iguales = [];
+  for(var i = 0; i < arreglo1.length; i++){
+    for(var j = 0; j < arreglo2.length; j++){
+      if(arreglo1[i] === arreglo2[j]){
+         iguales.push(arreglo1[i]);
+      }
+    }
+  }
+  return iguales;
 }
 
 
